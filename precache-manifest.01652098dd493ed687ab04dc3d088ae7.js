d8ca0d6f12888c6e54f42198a5270179929bd78d
Jaskadawn <j4sk4@hotmail.com>
2019-01-12 00:58:25 +0200

    fixed routing

---
+++ precache-manifest.01652098dd493ed687ab04dc3d088ae7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc64e9adf459b8b4974aa8531d8f95e3",
+    "url": "/swdb-release/static/media/background.cc64e9ad.jpg"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/swdb-release/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "a6ffc4a41b4dcacfecda",
+    "url": "/swdb-release/static/js/runtime~main.a6ffc4a4.js"
+  },
+  {
+    "revision": "f6a3f6821446b14e32ac",
+    "url": "/swdb-release/static/js/main.f6a3f682.chunk.js"
+  },
+  {
+    "revision": "9a2ef70d5fe2065a775b",
+    "url": "/swdb-release/static/js/1.9a2ef70d.chunk.js"
+  },
+  {
+    "revision": "f6a3f6821446b14e32ac",
+    "url": "/swdb-release/static/css/main.246c8e1f.chunk.css"
+  },
+  {
+    "revision": "699fae1b318dbb02c827e52fe431e41e",
+    "url": "/swdb-release/index.html"
+  }
+];
