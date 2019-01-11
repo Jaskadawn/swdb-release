e71e9097875f041945613aa66ccf80cdbb571355
Jaskadawn <j4sk4@hotmail.com>
2019-01-12 00:47:28 +0200

    Added website

---
+++ precache-manifest.f9267971015c07d43c1809600119a70e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc64e9adf459b8b4974aa8531d8f95e3",
+    "url": "/static/media/background.cc64e9ad.jpg"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "30904189ed5c62d9be99",
+    "url": "/static/js/main.30904189.chunk.js"
+  },
+  {
+    "revision": "9a2ef70d5fe2065a775b",
+    "url": "/static/js/1.9a2ef70d.chunk.js"
+  },
+  {
+    "revision": "30904189ed5c62d9be99",
+    "url": "/static/css/main.8efd39c3.chunk.css"
+  },
+  {
+    "revision": "cbc1ed11e4bf27cf08839c115f60a41b",
+    "url": "/index.html"
+  }
+];
